992f82d5be5c1806c4f2d09bf97e59e8fe8159e4
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2019-06-13 20:04:02 +0300

    add kek

--- tsp/base/graph.h
+++ tsp/base/graph.h
@@ -28,28 +28,17 @@ void init() {
   ASSERT(scanf("%d", &N), "cannot read from input");
   for (int i = 0; i < N; ++i) {
     ASSERT(scanf("%lf %lf", &points_[i][0], &points_[i][1]), "cannot read from input");
-    left_[i] = -1;
-    right_[i] = -1;
   }
   DBG("read grap size: " << N);
 }
 
 void set_rand_permutation() {
   PERF();
-  std::vector<int> perm(N);
   for (int i = 0; i < N; ++i) {
-    perm[i] = i;
+    perm_[i] = i;
   }
-  /*
   for (int i = 0; i < N; ++i) {
-    std::swap(perm[i], perm[(i + rnd()) % N]);
-  }
-  */
-
-  for (int i = 0; i < N; ++i) {
-    int v = perm[i];
-    left_[v] = perm[((i - 1) + N) % N];
-    right_[v] = perm[(i + 1) % N];
+    std::swap(perm_[i], perm_[(i + rnd()) % N]);
   }
 }
 
@@ -95,82 +84,44 @@ void set_2_optimal_permutation() {
       go(i);
     }
   }
-
-
-  for (int i = 0; i < N; ++i) {
-    int v = way_[i];
-    left_[v] = way_[((i - 1) + N) % N];
-    right_[v] = way_[(i + 1) % N];
-  }
-}
-
-void set_v(int v, short position) {
-  PERF();
-  left_[position] = v;
-  right_[v] = position;
 }
 
 void recalculate_distation() {
   PERF();
-  int now = 0;
   distation_ = 0;
-  do {
-    distation_ += dist(points_[now], points_[right_[now]]);
-    now = right_[now];
-  } while (now != 0);
+  for (int i = 0; i < N; ++i) {
+    distation_ += dist(points_[perm_[i]], points_[perm_[(i + 1) % N]]);
+  }
 }
 
 double swap_profit(int a, int b) {
   PERF();
-  if (left_[a] == b || right_[a] == b || a == b) {
+  if (a > b) {
+    std::swap(a, b);
+  }
+  if (perm_[((a - 1) % N + N) % N] == b) {
     return 0;
   }
-  /*
-  for (int i = 0; i < N; ++i) {
-    std::clog << "i: " << i << " point: " << points_[i][0] << ' ' << points_[i][1] << std::endl;
-  }
-  std::clog << "---------\n";
-  std::clog << "a : " << a << " b: " << b << std::endl;
-  std::clog << "a : " << points_[a][0] << ' ' << points_[a][1] <<
-      " b: " << points_[b][0] << ' ' << points_[b][1] << std::endl;
-  std::clog << "right_a : " << right_[a] << " left_b: " << left_[b] << std::endl;
-
-  std::clog << "right_a : " << points_[right_[a]][0] << ' ' << points_[right_[a]][1] <<
-      " left_b: " << points_[left_[b]][0] << ' ' << points_[left_[b]][1] << std::endl;
-
-  std::clog << "a : old_dist: " << dist(points_[a], points_[right_[a]]) <<
-      " new_dist: " << dist(points_[a], points_[left_[b]]) << std::endl;
-  */
-  return (
+  return
       (
-        dist(points_[a], points_[left_[a]]) +
-        dist(points_[a], points_[right_[a]])
-      ) - (
-        dist(points_[a], points_[left_[b]]) +
-        dist(points_[a], points_[right_[b]])
-      )
-  ) + (
+        dist(points_[perm_[a]], points_[perm_[((a - 1) % N + N) % N]]) +
+        dist(points_[perm_[b]], points_[perm_[((b + 1) % N + N) % N]])
+      ) - 
       (
-        dist(points_[b], points_[left_[b]]) +
-        dist(points_[b], points_[right_[b]])
-      ) - (
-        dist(points_[b], points_[left_[a]]) +
-        dist(points_[b], points_[right_[a]])
-      )
-  );
+        dist(points_[perm_[b]], points_[perm_[((a - 1) % N + N) % N]]) +
+        dist(points_[perm_[a]], points_[perm_[((b + 1) % N + N) % N]])
+      );
 }
 
 void swap(int a, int b, double profit) {
   PERF();
-  std::swap(left_[a], left_[b]);
-  std::swap(right_[a], right_[b]);
-
-  left_[right_[b]] = b;
-  right_[left_[b]] = b;
-
-  left_[right_[a]] = a;
-  right_[left_[a]] = a;
-
+  if (a > b) {
+    std::swap(a, b);
+  }
+  for (int i = a, j = b; i < j; ++i, --j) {
+    std::swap(perm_[i], perm_[j]);
+  }
+  //std::reverse(perm_ + a, perm_ + b + 1);
   distation_ -= profit;
 }
 
@@ -203,8 +154,9 @@ private:
 
   int N;
   double points_[kSize][2];
-  int left_[kSize];
-  int right_[kSize];
+
+  int perm_cnt{0};
+  int perm_[kSize];
   
   double distation_{0};
 
@@ -223,7 +175,7 @@ private:
 
   void go(int v) {
     used_[v] = true;
-    way_.push_back(v);
+    perm_[perm_cnt++] = v;
     for (auto to : G[v]) {
       if (!used_[to]) {
         go(to);
--- tsp/launcher.cpp
+++ tsp/launcher.cpp
@@ -6,37 +6,46 @@ Graph<20000> graph;
 int main() {
   graph.init();
 
-  graph.set_2_optimal_permutation();
+  graph.set_rand_permutation();
+  //graph.set_2_optimal_permutation();
   graph.recalculate_distation();
 
   std::cout << "distation: " << graph.distation() << std::endl;
 
-  int kMaxTemperature = 2e9;
-  for (int temperature = kMaxTemperature; temperature > 0; --temperature) {
+
+
+  double temp = 1000;
+  double coolingRate = 0.9999999;
+  while (temp > 1) {
     auto a = rnd() % graph.size();
     auto b = rnd() % graph.size();
     auto profit = graph.swap_profit(a, b);
-    if ((profit > 0)) {
+
+    double rnd_prob = (1.0 * (rnd())) / 10000.0 + 0.1;
+
+    if (profit > 0) {
       graph.swap(a, b, profit);
     }
-    if (temperature % 1000000 == 0) {
-      DBG("distation: " << graph.distation());
-    }
-    /*
     else if (
         profit != 0 &&
-        (-profit) / graph.distation() < (0.00001 * temperature / kMaxTemperature)
+        rnd_prob < exp((profit) / temp)
     ) {
       DBG(
-          "profit: " << profit <<
-          " ratio: " << (-profit) / graph.distation() <<
-          " threshold: " << (0.0001 * temperature / kMaxTemperature)
+          "distation: " << graph.distation() <<
+          " temp: " << temp <<
+          " a: " << a <<
+          " b: " << b << 
+          " accept prob: " << exp((profit) / temp) <<
+          " rnd_prob: " << rnd_prob <<
+          " profit: " << profit
       );
       graph.swap(a, b, profit);
     }
-  */
+
+    temp *= coolingRate;
   }
 
+  std::cout << "final: " << graph.distation() << std::endl;;
   graph.recalculate_distation();
   std::cout << "distation 2: " << graph.distation() << std::endl;
 
